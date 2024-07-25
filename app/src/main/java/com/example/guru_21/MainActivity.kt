deb96043f9e9207abca3cc2934e7adec1395e23c
JungBoHyun <wjdqhgus07@naver.com>
2024-07-25 16:29:10 +0900

    옵션 툴바 수정
    툴바 보이도록 수정
    버튼에서 옵션으로 수정

--- app/src/main/java/com/example/guru_21/MainActivity.kt
+++ app/src/main/java/com/example/guru_21/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.guru_21
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +13,6 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var loginButton: Button
-    lateinit var signupButton: Button
-    lateinit var mypageButton: Button
     lateinit var watchCourseButton: Button
     lateinit var makeCourseButton: Button
 
@@ -20,27 +20,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loginButton = findViewById<Button>(R.id.loginButton)
-        signupButton = findViewById<Button>(R.id.signupButton)
-        mypageButton = findViewById<Button>(R.id.mypageButton)
         watchCourseButton = findViewById<Button>(R.id.watchCourseButton)
         makeCourseButton = findViewById<Button>(R.id.makeCourseButton)
 
-        //로그인 창으로 이동하는 코드
-        loginButton.setOnClickListener{
-            var intent = Intent(this, loginActivity::class.java)
-            startActivity(intent)
-        }
-        //회원가입 창으로 이동하는 코드
-        signupButton.setOnClickListener{
-            var intent = Intent(this, signupActivity::class.java)
-            startActivity(intent)
-        }
-        //마이페이지 창으로 이동하는 코드
-        mypageButton.setOnClickListener{
-            var intent = Intent(this, mypageActivity::class.java)
-            startActivity(intent)
-        }
         //코스 살펴보기 창으로 이동하는 코드
         watchCourseButton.setOnClickListener{
             var intent = Intent(this, watchCourseActivity::class.java)
@@ -54,4 +36,30 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+    //로그인창으로 이동하는 코드
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.login -> {
+                val intent = Intent(this,loginActivity::class.java )
+                startActivity(intent)
+                return true
+            }
+            R.id.signup -> {
+                val intent = Intent(this,signupActivity::class.java )
+                startActivity(intent)
+                return true
+            }
+            R.id.mypage -> {
+                val intent = Intent(this,mypageActivity::class.java )
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
