dd6ab832f8310bd4feba43efaa22eb6704a36252
Junghyun Kim <hyunheee@swu.ac.kr>
2024-08-02 13:08:46 +0900

    MainActivity2삭제 및 coursepage 수정

--- app/src/main/java/com/example/guru_21/coursepageActivity.kt
+++ app/src/main/java/com/example/guru_21/coursepageActivity.kt
@@ -1,44 +1,20 @@
 package com.example.guru_21
 
-import android.database.Cursor
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.guru_21.databinding.ActivityCoursepageBinding
-import java.sql.Types.NULL
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class coursepageActivity : AppCompatActivity() {
-
-    lateinit var course1: TextView
-    lateinit var course2: TextView
-    lateinit var course3: TextView
-
-    lateinit var dbManager: MyDatabaseHelper
-    lateinit var sqlitedb: SQLiteDatabase
-
-    private lateinit var binding: ActivityCoursepageBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_coursepage)
-
-        course1 = findViewById(R.id.course1)
-        course2 = findViewById(R.id.course2)
-        course3 = findViewById(R.id.course3)
-
-        dbManager = MyDatabaseHelper(this, "tripDB", null, 1)
-        sqlitedb = dbManager.readableDatabase
-
-        var cursor: Cursor
-        cursor = sqlitedb.rawQuery("SELECT * FROM mycourse;", null)
-
-
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
+}
